d1d28b9dfdf43efd88099135f616c58cc34f039d
Gang Li <ligang@orby.ai>
2025-05-13 21:23:14 +0000

    Add init script

---
+++ mosaic/init_interactive.sh
@@ -0,0 +1,16 @@
+apt update
+apt install -y emacs
+apt install -y awscli
+pip install -y parquet-tools
+
+# Download model.
+python3 -c "import transformers; transformers.pipeline(model='Qwen/Qwen2.5-VL-7B-Instruct')"
+
+# Install verl lib: https://verl.readthedocs.io/en/latest/start/install.html
+pip3 install -e .[vllm]
+
+# Download and convert uground dev set
+# mkdir -p ~/data/uground/raw/
+# aws s3 cp s3://orby-osu-va/mds_datasets/Q42024_Intake_Format/ActIO-ActionDescription/parquet/dev.parquet ~/data/uground/raw/dev.parquet
+# python examples/data_preprocess/uground.py --input_file=~/data/uground/raw/dev.parquet --split=train
+# python examples/data_preprocess/uground.py --input_file=~/data/uground/raw/dev.parquet --split=test
