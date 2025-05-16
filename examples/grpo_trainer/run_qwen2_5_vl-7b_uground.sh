2effb6f84e0c6c340d8a9f53a67319138ee0a69b
Gang Li <ligang@orby.ai>
2025-05-16 18:56:00 +0000

    Update uground script

--- examples/grpo_trainer/run_qwen2_5_vl-7b_uground.sh
+++ examples/grpo_trainer/run_qwen2_5_vl-7b_uground.sh
@@ -8,14 +8,14 @@ python3 -m verl.trainer.main_ppo \
     data.train_files=$HOME/data/uground/train.parquet \
     data.val_files=$HOME/data/uground/test.parquet \
     data.train_batch_size=8 \
-    data.max_prompt_length=4096 \
+    data.max_prompt_length=8192 \
     data.max_response_length=512 \
     data.filter_overlong_prompts=True \
     data.truncation='right' \
     data.image_key=images \
     actor_rollout_ref.model.path=Qwen/Qwen2.5-VL-7B-Instruct \
     actor_rollout_ref.actor.optim.lr=1e-6 \
-    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.model.use_remove_padding=False \
     actor_rollout_ref.actor.ppo_mini_batch_size=8 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.actor.use_kl_loss=True \
@@ -35,6 +35,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.n=1 \
     actor_rollout_ref.rollout.max_model_len=8192 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
+    actor_rollout_ref.ref.log_prob_max_token_len_per_gpu=8192 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.use_kl_in_reward=False \
     trainer.critic_warmup=0 \
