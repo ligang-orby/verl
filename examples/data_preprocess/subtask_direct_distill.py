a743bda8e70acbb8bf5bf710630fae70a27f3f1b
Cheng Chang <cheng@orby.ai>
2025-05-20 15:33:47 -0700

    Update subtask_direct_distill.py

--- examples/data_preprocess/subtask_direct_distill.py
+++ examples/data_preprocess/subtask_direct_distill.py
@@ -3,14 +3,50 @@ Preprocess the subtask SVA v3 dataset to parquet format
 """
 
 import argparse
+from typing import TypedDict
 
 import boto3
+import pandas as pd
 import ray
+from fm.action_data_pb2 import ActionData
+from PIL import Image
 from tqdm import tqdm
 
 from .utils import s3_utils
 
 ray.init()
+
+
+class PromptDict(TypedDict):
+    role: str
+    content: str
+
+
+class RewardModelDict(TypedDict):
+    style: str = "rule"
+    ground_truth: dict
+
+
+class VERLDataPoint(TypedDict):
+    data_source: str = "subtask_direct_distill"
+    prompt: list[PromptDict]
+    images: list[Image.Image]
+    ability: str = "vision"
+    reward_model: RewardModelDict
+    extra_info: dict
+
+
+def convert_action_to_datapoint(action: ActionData) -> VERLDataPoint:
+    """
+    Convert individual action to a VERLDataPoint.
+
+    Args:
+        action (ActionData): The action to convert.
+
+    Returns:
+        VERLDataPoint: The converted data point.
+    """
+    pass
 
 
 @ray.remote
@@ -25,7 +61,17 @@ def data_processing_task(pb_uris_batch: list[str], output_path: str) -> int:
     Returns:
         int: The number of data points created in the parquet file.
     """
-    pass
+    for pb_uri in pb_uris_batch:
+        td = s3_utils.load_trajectory_data_from_s3(pb_uri)
+
+        data_list: list[VERLDataPoint] = []
+        for action in td.actions:
+            data = convert_action_to_datapoint(action)
+            data_list.append(data)
+
+        data_df = pd.DataFrame(data_list)
+        data_df.to_parquet(output_path, index=False)
+        return len(data_list)
 
 
 def main(input_path: str, output_path: str) -> None:
