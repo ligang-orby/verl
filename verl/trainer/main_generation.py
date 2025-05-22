bf2a149c8af7a30a6fa123eb795d87ae84a27929
Gang Li <ligang@orby.ai>
2025-05-22 11:46:35 -0700

    Update main generation for image input

--- verl/trainer/main_generation.py
+++ verl/trainer/main_generation.py
@@ -29,15 +29,44 @@ from pprint import pprint
 
 import pandas as pd
 from omegaconf import OmegaConf
+from torchdata.stateful_dataloader import StatefulDataLoader
 
 from verl import DataProto
 from verl.protocol import pad_dataproto_to_divisor, unpad_dataproto
-from verl.single_controller.ray import RayClassWithInitArgs, RayResourcePool, RayWorkerGroup
-from verl.utils import hf_tokenizer
+from verl.single_controller.ray import (
+    RayClassWithInitArgs,
+    RayResourcePool,
+    RayWorkerGroup,
+)
+from verl.utils import hf_tokenizer, hf_processor
 from verl.utils.fs import copy_to_local
 from verl.utils.hdfs_io import makedirs
 from verl.utils.model import compute_position_id_with_mask
 from verl.workers.fsdp_workers import ActorRolloutRefWorker
+from verl.utils.dataset.rl_dataset import collate_fn as default_collate_fn
+from verl.trainer.main_ppo import create_rl_dataset
+
+
+def _create_dataloader(config, tokenizer, processor):
+    """
+    Creates the dataloader.
+    """
+    dataset = create_rl_dataset(config.data.path, config.data, tokenizer, processor)
+
+    dataloader = StatefulDataLoader(
+        dataset=dataset,
+        batch_size=config.data.batch_size,
+        num_workers=config.data.get("dataloader_num_workers", 8),
+        shuffle=False,
+        drop_last=False,
+        collate_fn=default_collate_fn,
+    )
+
+    assert len(dataloader) >= 1, "Dataloader is empty!"
+
+    print(f"Size of dataloader: {len(dataloader)}")
+
+    return dataloader
 
 
 @hydra.main(config_path="config", config_name="generation", version_base=None)
@@ -49,7 +78,9 @@ def run_generation(config) -> None:
     if not ray.is_initialized():
         # this is for local ray cluster
         ray.init(
-            runtime_env={"env_vars": {"TOKENIZERS_PARALLELISM": "true", "NCCL_DEBUG": "WARN"}},
+            runtime_env={
+                "env_vars": {"TOKENIZERS_PARALLELISM": "true", "NCCL_DEBUG": "WARN"}
+            },
             num_cpus=config.ray_init.num_cpus,
         )
 
@@ -58,12 +89,20 @@ def run_generation(config) -> None:
 
 @ray.remote(num_cpus=1)
 def main_task(config):
-    pprint(OmegaConf.to_container(config, resolve=True))  # resolve=True will eval symbol values
+    pprint(
+        OmegaConf.to_container(config, resolve=True)
+    )  # resolve=True will eval symbol values
     OmegaConf.resolve(config)
 
     local_path = copy_to_local(config.model.path)
     trust_remote_code = config.data.get("trust_remote_code", False)
     tokenizer = hf_tokenizer(local_path, trust_remote_code=trust_remote_code)
+    processor = hf_processor(
+        local_path, use_fast=True
+    )  # used for multimodal LLM, could be none
+
+    dataloader = _create_dataloader(config, tokenizer, processor)
+    print(dataloader)
 
     if config.rollout.temperature == 0.0:
         assert config.data.n_samples == 1, "When temperature=0, n_samples must be 1."
@@ -79,9 +118,15 @@ def main_task(config):
     if tokenizer.pad_token is None:
         tokenizer.pad_token = tokenizer.eos_token
 
-    ray_cls_with_init = RayClassWithInitArgs(cls=ray.remote(ActorRolloutRefWorker), config=config, role="rollout")
-    resource_pool = RayResourcePool(process_on_nodes=[config.trainer.n_gpus_per_node] * config.trainer.nnodes)
-    wg = RayWorkerGroup(resource_pool=resource_pool, ray_cls_with_init=ray_cls_with_init)
+    ray_cls_with_init = RayClassWithInitArgs(
+        cls=ray.remote(ActorRolloutRefWorker), config=config, role="rollout"
+    )
+    resource_pool = RayResourcePool(
+        process_on_nodes=[config.trainer.n_gpus_per_node] * config.trainer.nnodes
+    )
+    wg = RayWorkerGroup(
+        resource_pool=resource_pool, ray_cls_with_init=ray_cls_with_init
+    )
     wg.init_model()
 
     total_samples = len(dataset)
@@ -89,6 +134,52 @@ def main_task(config):
     num_batch = -(-total_samples // config_batch_size)
     output_lst = [[] for _ in range(config.data.n_samples)]
 
+    for test_data in dataloader:
+        test_batch = DataProto.from_single_dict(test_data)
+
+        # Store original inputs
+        input_ids = test_batch.batch["input_ids"]
+
+        batch_keys_to_pop = ["input_ids", "attention_mask", "position_ids"]
+        non_tensor_batch_keys_to_pop = ["raw_prompt_ids"]
+        if "multi_modal_inputs" in test_batch.non_tensor_batch:
+            non_tensor_batch_keys_to_pop.extend(
+                ["multi_modal_data", "multi_modal_inputs"]
+            )
+        if "raw_prompt" in test_batch.non_tensor_batch:
+            non_tensor_batch_keys_to_pop.append("raw_prompt")
+        if "tools_kwargs" in test_batch.non_tensor_batch:
+            non_tensor_batch_keys_to_pop.append("tools_kwargs")
+        test_gen_batch = test_batch.pop(
+            batch_keys=batch_keys_to_pop,
+            non_tensor_batch_keys=non_tensor_batch_keys_to_pop,
+        )
+
+        test_gen_batch.meta_info = {
+            "eos_token_id": tokenizer.eos_token_id,
+            "pad_token_id": tokenizer.pad_token_id,
+            "recompute_log_prob": False,
+            "do_sample": False,
+            "validate": True,
+        }
+        print(f"test_gen_batch meta info: {test_gen_batch.meta_info}")
+
+        # pad to be divisible by dp_size
+        batch_padded, pad_size = pad_dataproto_to_divisor(test_gen_batch, 1)
+        output_padded = wg.generate_sequences(batch_padded)
+
+        # unpad
+        output_gen_batch = unpad_dataproto(output_padded, pad_size=pad_size)
+        print("validation generation end")
+
+        # Store generated outputs
+        output_ids = output_gen_batch.batch["responses"]
+        output_texts = [
+            tokenizer.decode(ids, skip_special_tokens=True) for ids in output_ids
+        ]
+        print(output_texts)
+
+    """
     for batch_idx in range(num_batch):
         print(f"[{batch_idx + 1}/{num_batch}] Start to process.")
         batch_chat_lst = chat_lst[batch_idx * config_batch_size : (batch_idx + 1) * config_batch_size]
@@ -138,6 +229,7 @@ def main_task(config):
     output_dir = os.path.dirname(config.data.output_path)
     makedirs(output_dir, exist_ok=True)
     dataset.to_parquet(config.data.output_path)
+    """
 
 
 if __name__ == "__main__":
