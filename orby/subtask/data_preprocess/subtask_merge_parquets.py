932a7f34bac28c821d9fca2a63134c1425d7851f
Cheng Chang <cheng@orby.ai>
2025-05-23 16:17:37 -0700

    Update subtask_merge_parquets.py

--- orby/subtask/data_preprocess/subtask_merge_parquets.py
+++ orby/subtask/data_preprocess/subtask_merge_parquets.py
@@ -2,15 +2,20 @@ import time
 
 import pandas as pd
 
-S3_PARQUET_SOURCE_DIR = "s3://orby-osu-va/subtask/verl/experiment_2/test/reward_model/"
-S3_PARQUET_OUTPUT_PATH = "s3://orby-osu-va/subtask/verl/experiment_2/test/reward_model.parquet"
+S3_PARQUET_PAIRS = [
+    ["s3://orby-osu-va/subtask/verl/experiment_2/test/reward_model/", "s3://orby-osu-va/subtask/verl/experiment_2/test/reward_model.parquet"],
+    ["s3://orby-osu-va/subtask/verl/experiment_2/test/executor/", "s3://orby-osu-va/subtask/verl/experiment_2/test/executor.parquet"],
+    ["s3://orby-osu-va/subtask/verl/experiment_2/train/reward_model/", "s3://orby-osu-va/subtask/verl/experiment_2/train/reward_model.parquet"],
+    ["s3://orby-osu-va/subtask/verl/experiment_2/train/executor/", "s3://orby-osu-va/subtask/verl/experiment_2/train/executor.parquet"],
+]
 
 
 def main():
-    start_time = time.time()
-    df = pd.read_parquet(S3_PARQUET_SOURCE_DIR)
-    df.to_parquet(S3_PARQUET_OUTPUT_PATH)
-    print(f"Took {(time.time() - start_time) / 60:.1f} minutes")
+    for source_dir, output_path in S3_PARQUET_PAIRS:
+        start_time = time.time()
+        df = pd.read_parquet(source_dir)
+        df.to_parquet(output_path)
+        print(f"${source_dir} took {(time.time() - start_time) / 60:.1f} minutes")
     print("Done!")
 
 
