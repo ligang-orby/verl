b271c1e523906bb718b31d0ec03e7bf0853562fa
Cheng Chang <cheng@orby.ai>
2025-05-22 19:23:37 -0700

    sh change

--- mosaic/init_interactive.sh
+++ mosaic/init_interactive.sh
@@ -19,3 +19,13 @@ pip3 install -e .[vllm]
 # aws s3 cp s3://orby-osu-va/mds_datasets/Q42024_Intake_Format/ActIO-ActionDescription/parquet/dev.parquet ~/data/uground/raw/dev.parquet
 # python examples/data_preprocess/uground.py --input_file=~/data/uground/raw/dev.parquet --split=train
 # python examples/data_preprocess/uground.py --input_file=~/data/uground/raw/dev.parquet --split=test
+
+# Download the subtask direct distill dataset
+mkdir -p ~/data/subtask_direct_distill/mix/train/executor/
+mkdir -p ~/data/subtask_direct_distill/mix/test/executor/
+mkdir -p ~/data/subtask_direct_distill/mix/train/reward_model/
+mkdir -p ~/data/subtask_direct_distill/mix/test/reward_model/
+aws s3 cp s3://orby-osu-va/subtask/verl/experiment_2/test/executor/ ~/data/subtask_direct_distill/mix/test/executor/ --recursive
+aws s3 cp s3://orby-osu-va/subtask/verl/experiment_2/test/reward_model/ ~/data/subtask_direct_distill/mix/test/reward_model/ --recursive
+aws s3 cp s3://orby-osu-va/subtask/verl/experiment_2/train/executor/ ~/data/subtask_direct_distill/mix/train/executor/ --recursive
+aws s3 cp s3://orby-osu-va/subtask/verl/experiment_2/train/reward_model/ ~/data/subtask_direct_distill/mix/train/reward_model/ --recursive
